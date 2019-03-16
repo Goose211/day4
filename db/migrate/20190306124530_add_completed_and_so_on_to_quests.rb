b65f7ce45b5ca2b6adb40d5e1218584c6897a4f9
Goose211 <211suzuken@gmail.com>
2019-03-16 12:56:45 +0000

    Fix due_date nem

--- db/migrate/20190306124530_add_completed_and_so_on_to_quests.rb
+++ db/migrate/20190306124530_add_completed_and_so_on_to_quests.rb
@@ -1,6 +1,6 @@
 class AddCompletedAndSoOnToQuests < ActiveRecord::Migration[5.2]
   def change
     add_column :quests, :completed, :boolean
-    add_column :quests, :due_date, :due_date
+    add_column :quests, :due_date, :date
   end
 end
